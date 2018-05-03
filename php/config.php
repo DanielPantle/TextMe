8da2202b99a31fa5f0bea84337c54a60eb546f65
Daniel <dan.pantle.iel@gmx.de>
2018-05-03 12:50:33 +0200

    config.php file added

---
+++ php/config.php
@@ -0,0 +1,15 @@
+<?php
+
+if(!defined("MYSQL_HOST"))
+	define("MYSQL_HOST", "localhost");
+
+if(!defined("MYSQL_USER"))
+	define("MYSQL_USER", "root");
+
+if(!defined("MYSQL_PASSWORD"))
+	define("MYSQL_PASSWORD", "");
+
+if(!defined("MYSQL_DB"))
+	define("MYSQL_DB", "TextMe");
+
+?>
