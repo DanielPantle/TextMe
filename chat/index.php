6feafd6e8929602dbf6b5829f8a72908bd336410
schoofsc <schoofsc@googlemail.com>
2018-05-03 17:14:06 +0200

    - Ordner Strucktur hergestellt - dateien der Mainpage umbenannt      - layout.css in mainpage.css      - layout.js in mainpage.js - /chat/index.php hinzugefügt als Hauptseite nach dem Login

---
+++ chat/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TextMe</title>
+</head>
+<body>
+<!-- Hier den Code für die Main Page-->
+<?php
+
+?>
+</body>
+</html>
