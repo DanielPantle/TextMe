017f9c0c7039bed1b3e3f23bdda3e91e7920aa7e
Daniel <dan.pantle.iel@gmx.de>
2018-05-07 09:50:41 +0200

    PHP-Config-Datei geändert

--- php/config.php
+++ php/config.php
@@ -1,15 +1,8 @@
 <?php
 
-if(!defined("MYSQL_HOST"))
-	define("MYSQL_HOST", "localhost");
-
-if(!defined("MYSQL_USER"))
-	define("MYSQL_USER", "root");
-
-if(!defined("MYSQL_PASSWORD"))
-	define("MYSQL_PASSWORD", "");
-
-if(!defined("MYSQL_DB"))
-	define("MYSQL_DB", "TextMe");
+define("MYSQL_HOST", "localhost");
+define("MYSQL_USER", "root");
+define("MYSQL_PASSWORD", "");
+define("MYSQL_DB", "TextMe");
 
 ?>
