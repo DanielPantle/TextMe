88147cfe58a0b44e974f9016dad7ba0860a8930d
schoofsc <schoofsc@googlemail.com>
2018-05-07 14:09:13 +0200

    Datenbankuser geändert

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
 
 define("MYSQL_HOST", "localhost");
-define("MYSQL_USER", "root");
-define("MYSQL_PASSWORD", "");
+define("MYSQL_USER", "TextMe");
+define("MYSQL_PASSWORD", "Pineapple");
 define("MYSQL_DB", "TextMe");
 define("MYSQL_PORT", "3306");
 
