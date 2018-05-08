fe8236c882bb1b382af24f0eb0c9cf470d5062de
schoofsc <schoofsc@googlemail.com>
2018-05-08 13:01:08 +0200

    Logout Button / Nutzernamen

--- php/db.php
+++ php/db.php
@@ -179,32 +179,6 @@ class Database {
 			return "Error: " . $e->getMessage();
 		}
 	}
-	public function getAllChatsFromCurrentUser() {
-		try {
-			$currentUser = $this->getCurrentUser();
-
-			$stmt = $this->db->prepare("SELECT {$this->C_ID} AS cid, {$this->C_NAME} AS chatname, GROUP_CONCAT({$this->U_NAME}) AS members
-					FROM {$this->TABLE_CHAT}, {$this->TABLE_USER_IS_IN_CHAT}, {$this->TABLE_USER}
-					WHERE {$this->UIIC_CID} = {$this->C_ID}
-					AND {$this->UIIC_UID} = {$this->U_ID}
-					GROUP BY {$this->C_ID}
-					HAVING members LIKE '%$currentUser%'");
-
-			if($stmt->execute()) {
-				return $stmt->fetchAll(PDO::FETCH_ASSOC);
-			}
-			else {
-				return false;
-			}
-		}
-		catch(PDOException $e) {
-			return "Error: " . $e->getMessage();
-		}
-	}
-
-	public function getCurrentUser() {
-		return $_SESSION['name'];
-	}
 
 	public function getAllChatsFromCurrentUser() {
 		try {
