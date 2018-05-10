46e0c198e3f1aa3a56a502bf5eda1d9ddc7a814b
floriangallik <florian.gallik@mercatis.com>
2018-05-10 23:05:14 +0200

    add tons of validation stuff

--- js/login-signup.js
+++ js/login-signup.js
@@ -15,40 +15,143 @@ $(function() {
         e.preventDefault();
     });
 
+    validateLogin();
+    validateRegistration();
+
+});
+
 /**
- * check username length and password length from login and prevent post by fail
+ * validates username and password
  * */
+function validateLogin() {
+
+    const loginUsernameField = $("#login-username");
+    const loginPasswordField = $("#login-password");
+
     $('.btn-login').click(function (e) {
-        const username = $("#username").val();
-        const password = $("#password").val();
+        const username = loginUsernameField.val();
+        const password = loginPasswordField.val();
         if(username.length < 3) {
             e.preventDefault();
             console.log("username error");
-            $("#username").addClass("invalid");
-            jQuery("#username-error").toggle();
+            loginUsernameField.addClass("invalid");
+            jQuery("#username-error").show();
         }
         if(password.length < 6){
             e.preventDefault();
             console.log("password error");
-            $("#password").addClass("invalid");
-            jQuery("#password-error").toggle();
+            loginPasswordField.addClass("invalid");
+            jQuery("#password-error").show();
         }
         console.log("username: " + username + ", pw: " + password);
     });
 
-
-    $("#username").click(function () {
-        if($("#username").hasClass("invalid")) {
-            $("#username").removeClass("invalid");
-            jQuery("#username-error").toggle();
+    loginUsernameField.keypress(function () {
+        if(loginUsernameField.hasClass("invalid")) {
+            loginUsernameField.removeClass("invalid");
+            jQuery("#username-error").hide();
         }
     });
 
-    $("#password").click(function () {
-        if($("#password").hasClass("invalid")) {
-            $("#password").removeClass("invalid");
-            jQuery("#password-error").toggle();
+    loginPasswordField.keypress(function () {
+        if(loginPasswordField.hasClass("invalid")) {
+            loginPasswordField.removeClass("invalid");
+            jQuery("#password-error").hide();
+        }
+    });
+}
+
+/**
+ * validates every input field
+ */
+function validateRegistration() {
+
+    const registerUsernameField = $("#register-username");
+    const registerPasswordField = $("#register-password");
+    const registerEmailField = $("#email");
+    const registerPasswordConfirmField = $("#confirm-password");
+
+    $('.btn-register').click(function (e) {
+        const username = registerUsernameField.val();
+        const password = registerPasswordField.val();
+        const passwordConfirm = registerPasswordConfirmField.val();
+        const email  = registerEmailField.val();
+
+        if(username.length < 3) {
+            e.preventDefault();
+            console.log("username error");
+            registerUsernameField.addClass("invalid");
+            jQuery("#register-username-error").show();
+        }
+        if(!isValidEmailAddress(email)) {
+            e.preventDefault();
+            console.log("email error");
+            registerEmailField.addClass("invalid");
+            jQuery("#register-email-error").show();
+        }
+
+        const passwordMessage = validatePassword(password);
+        if(passwordMessage !== ""){
+            e.preventDefault();
+            console.log(passwordMessage);
+            registerPasswordField.addClass("invalid");
+            jQuery("#register-password-error").html(passwordMessage);
+            jQuery("#register-password-error").show();
+        }
+        if(passwordConfirm !== password){
+            e.preventDefault();
+            console.log("password confirm error");
+            registerPasswordConfirmField.addClass("invalid");
+            jQuery("#register-password-confirm-error").show();
         }
     });
 
-});
+    registerUsernameField.keypress(function () {
+        if(registerUsernameField.hasClass("invalid")) {
+            registerUsernameField.removeClass("invalid");
+            jQuery("#register-username-error").hide();
+        }
+    });
+    registerEmailField.keypress(function () {
+        if(registerEmailField.hasClass("invalid")) {
+            registerEmailField.removeClass("invalid");
+            jQuery("#register-email-error").hide();
+        }
+    });
+    registerPasswordField.keypress(function () {
+        if(registerPasswordField.hasClass("invalid")) {
+            registerPasswordField.removeClass("invalid");
+            jQuery("#register-password-error").hide();
+        }
+    });
+    registerPasswordConfirmField.keypress(function () {
+        if(registerPasswordConfirmField.hasClass("invalid")) {
+            registerPasswordConfirmField.removeClass("invalid");
+            jQuery("#register-password-confirm-error").hide();
+        }
+    });
+
+
+    function isValidEmailAddress(emailAddress) {
+        var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        return pattern.test(emailAddress);
+    }
+
+    function validatePassword(str) {
+            let returnMessage = "";
+            if (str.length < 6) {
+                returnMessage = returnMessage.concat("<li>must be at least 6 characters</li><br>");
+            } if (str.search(/\d/) == -1) {
+                returnMessage = returnMessage.concat("<li>must contain at least 1 digit</li><br>");
+            } if (str.search(/[A-Z]/) == -1) {
+                returnMessage = returnMessage.concat("<li>must contain at least 1 upper case letter</li><br>");
+            } if (str.search(/[a-z]/) == -1) {
+                returnMessage = returnMessage.concat("<li>must contain at least 1 lower case letter</li><br>");
+            } if (str.search(/[!"#\$%&'\(\)\*\+,\-\.\/:;<=>\?@\[\]\^_`{\|}~]/) == -1) {
+                returnMessage = returnMessage.concat("<li>must contain at least 1 special character</li>");
+            }
+            return(returnMessage);
+    }
+
+
+}
