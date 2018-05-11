c38f9d5a2509327d04a46d6b7d09ae9a4d125cfb
floriangallik <florian.gallik@mercatis.com>
2018-05-11 23:26:17 +0200

    same

--- index.php
+++ index.php
@@ -67,6 +67,7 @@ if($Database->isLoggedIn()) {
 
 <head>
     <title>TextMe</title>
+
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="css/login-signup.css">
     <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>-->
